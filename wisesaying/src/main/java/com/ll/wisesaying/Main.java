34cf46e7be9ae149c346f694b909e1e6556bd158
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 15:31:23 +0900

    feat: app run

--- wisesaying/src/main/java/com/ll/wisesaying/Main.java
+++ wisesaying/src/main/java/com/ll/wisesaying/Main.java
@@ -1,4 +1,11 @@
 package com.ll.wisesaying;
 
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new App().run();
+    }
+
 }
