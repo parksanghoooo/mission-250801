6265693c50610eb9208f8134d1b4844d21af0418
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 15:47:53 +0900

    refactor: message format

--- wisesaying/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
+++ wisesaying/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
@@ -6,8 +6,8 @@ public class ErrorMessage {
      * [500 INTERNAL_SERVER_ERROR]
      * - 서버 오류
      */
-    public static final String FAIL_TO_LOAD_DIRECTORY = "명언 폴더 로딩 실패";
-    public static final String FAIL_TO_LOAD_FILE = "명언 파일 로딩 실패";
+    public static final String FAIL_TO_LOAD_DIRECTORY = "[명언 폴더 로딩 실패] 폴더명 : { %s }\n";
+    public static final String FAIL_TO_LOAD_FILE = "[명언 파일 로딩 실패] 파일명 : { %s }\n";
     public static final String FAIL_TO_SAVE_FILE = "명언 파일 저장 실패";
     public static final String FAIL_TO_DELETE_FILE = "명언 파일 삭제 실패";
     public static final String FAIL_TO_LOAD_LAST_ID = "lastId.txt 로딩 실패";
