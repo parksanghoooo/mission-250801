1e47fed28aa74121817e2d8c73c0c84d398f9343
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 15:31:48 +0900

    feat: error 관련 constant

---
+++ wisesaying/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.ll.wisesaying.global.constant;
+
+public class ErrorMessage {
+
+    /**
+     * [500 INTERNAL_SERVER_ERROR]
+     * - 서버 오류
+     */
+    public static final String FAIL_TO_LOAD_DIRECTORY = "명언 폴더 로딩 실패";
+    public static final String FAIL_TO_LOAD_FILE = "명언 파일 로딩 실패";
+    public static final String FAIL_TO_SAVE_FILE = "명언 파일 저장 실패";
+    public static final String FAIL_TO_DELETE_FILE = "명언 파일 삭제 실패";
+    public static final String FAIL_TO_LOAD_LAST_ID = "lastId.txt 로딩 실패";
+    public static final String FAIL_TO_WRITE_LAST_ID = "lastId.txt 저장 실패";
+
+}
