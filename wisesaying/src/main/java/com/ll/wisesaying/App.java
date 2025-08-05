b3b685ed06a9937cacddb40095494744e5f9f644
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 12:22:54 +0900

    feat: 종료 기능

---
+++ wisesaying/src/main/java/com/ll/wisesaying/App.java
@@ -0,0 +1,30 @@
+package com.ll.wisesaying;
+
+import com.ll.wisesaying.global.constant.Command;
+import com.ll.wisesaying.global.constant.Message;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+
+    private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public void run() throws IOException {
+
+        System.out.println(Message.APP_TITLE);
+
+        while (true) {
+
+            System.out.println(Message.INPUT);
+            String cmd = br.readLine().trim();
+
+            // 종료
+            if (cmd.equals(Command.QUIT)) break;
+
+        }
+
+    }
+
+}
