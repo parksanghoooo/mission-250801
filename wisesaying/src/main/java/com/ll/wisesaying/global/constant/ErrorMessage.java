50e3a5131cf220238d5c25d2c64a35e731bd36d8
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 16:37:05 +0900

    feat: build fail response

--- wisesaying/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
+++ wisesaying/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
@@ -12,11 +12,11 @@ public class ErrorMessage {
      * [500 INTERNAL_SERVER_ERROR]
      * - 서버 오류
      */
-    public static final String FAIL_TO_LOAD_DIRECTORY = "[명언 폴더 로딩 실패] 폴더명 : { %s }\n";
-    public static final String FAIL_TO_LOAD_FILE = "[명언 파일 로딩 실패] 파일명 : { %s }\n";
     public static final String FAIL_TO_SAVE_FILE = "명언 파일 저장 실패";
     public static final String FAIL_TO_DELETE_FILE = "[명언 파일 삭제 실패] 파일명 : { %s.json }\n";
-    public static final String FAIL_TO_LOAD_LAST_ID = "lastId.txt 로딩 실패";
-    public static final String FAIL_TO_WRITE_LAST_ID = "lastId.txt 저장 실패";
+    public static final String FAIL_TO_LOAD_LAST_ID_FILE = "lastId.txt 로딩 실패";
+    public static final String FAIL_TO_SAVE_LAST_ID_FILE = "lastId.txt 저장 실패";
+    public static final String FAIL_TO_LOAD_BUILD_FILE = "data.json 로딩 실패";
+    public static final String FAIL_TO_SAVE_BUILD_FILE = "data.json 생성 실패";
 
 }
