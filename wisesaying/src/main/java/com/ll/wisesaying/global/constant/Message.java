03c4ef2ded0600dbf8c4842c600c46c04e4c172f
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 15:48:15 +0900

    feat: 목록 관련 message

--- wisesaying/src/main/java/com/ll/wisesaying/global/constant/Message.java
+++ wisesaying/src/main/java/com/ll/wisesaying/global/constant/Message.java
@@ -6,10 +6,15 @@ public class Message {
     public static final String APP_TITLE = "== 명언 앱 ==";
     public static final String INPUT = "명령) ";
 
-    // 입력
+    // 입력 관련
     public static final String INPUT_CONTENT = "명언 : ";
     public static final String INPUT_AUTHOR = "작가 : ";
 
+    // 등록 관련
+    public static final String REGISTER_SUCCESS = "%d번 명언이 등록되었습니다.\n";
 
+    // 목록 관련
+    public static final String LIST_HEADER = "번호 / 작가 / 명언\n----------------------";
+    public static final String LIST_ROW_FORMAT = "%d / %s / %s\n";
 
 }
