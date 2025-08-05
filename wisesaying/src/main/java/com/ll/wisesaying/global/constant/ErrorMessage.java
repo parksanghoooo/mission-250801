7ba4e46a3c5455d23224aea6f67ecdf8dc393b97
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 16:10:25 +0900

    feat: not exist wise saying error message

--- wisesaying/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
+++ wisesaying/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
@@ -3,13 +3,19 @@ package com.ll.wisesaying.global.constant;
 public class ErrorMessage {
 
     /**
+     * [404 Not Found]
+     * - 존재하지 않는 자원
+     */
+    public static final String NOT_EXIST_WISE_SAYING = "%d번 명언은 존재하지 않습니다.\n";
+
+    /**
      * [500 INTERNAL_SERVER_ERROR]
      * - 서버 오류
      */
     public static final String FAIL_TO_LOAD_DIRECTORY = "[명언 폴더 로딩 실패] 폴더명 : { %s }\n";
     public static final String FAIL_TO_LOAD_FILE = "[명언 파일 로딩 실패] 파일명 : { %s }\n";
     public static final String FAIL_TO_SAVE_FILE = "명언 파일 저장 실패";
-    public static final String FAIL_TO_DELETE_FILE = "명언 파일 삭제 실패";
+    public static final String FAIL_TO_DELETE_FILE = "[명언 파일 삭제 실패] 파일명 : { %s.json }\n";
     public static final String FAIL_TO_LOAD_LAST_ID = "lastId.txt 로딩 실패";
     public static final String FAIL_TO_WRITE_LAST_ID = "lastId.txt 저장 실패";
 
