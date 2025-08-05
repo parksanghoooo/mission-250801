c298359c03bfb0e9800954237862d9c27ce8cd36
SangHuPark <tkdgn5408407@gmail.com>
2025-08-05 15:09:55 +0900

    feat: object mapper dependency

--- wisesaying/build.gradle.kts
+++ wisesaying/build.gradle.kts
@@ -10,6 +10,13 @@ repositories {
 }
 
 dependencies {
+
+    /**
+     * Util 의존성
+     * - Object Mapper
+     */
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
